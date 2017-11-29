dd1686d9783857fe435ab567ab8fdc1b98ef00b9
rederry <rederry@outlook.com>
2017-11-29 10:30:26 +0800

    finish Chapter2.4

---
+++ Chapter2.4/IndexMinPQ.java
@@ -0,0 +1,23 @@
+public class IndexMinPQ {
+    
+    public IndexMinPQ(int MaxN) {
+    }
+
+    public void insert(int k, Item item) {
+    }
+
+    public void change(int k, Item item) {
+    }
+
+    public boolean contains(int k) {
+    }
+
+    public void delete(int k) {
+    }
+
+    public Item min() {
+    }
+
+
+    
+}
